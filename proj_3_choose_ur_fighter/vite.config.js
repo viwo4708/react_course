fa43861ec6f28a370523603b1e47b663fc0f0884
viwo4708 <viwo4708@colorado.edu>
2023-01-06 17:44:57 -0700

    file path

--- proj_3_choose_ur_fighter/vite.config.js
+++ proj_3_choose_ur_fighter/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react_course/proj_3_choose_ur_fighter/'
+  base: './react_course/proj_3_choose_ur_fighter/'
 })
